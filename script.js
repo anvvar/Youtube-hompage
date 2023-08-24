f4f6dca37f9b72ba1eac18ddecc08ec7bdd0910f
anwar <anwarkp.qw@gmail.com>
2023-08-24 20:55:52 +0530

    fixed some responsive layout issues

--- script.js
+++ script.js
@@ -1,64 +1,97 @@
+function handleScreenSizeChange() {
+  if (window.innerWidth >= 1300) {
+    functionForLargeScreen();
+  } else {
+    functionForSmallScreen();
+  }
+}
 
+function functionForLargeScreen() {
+  window.onload = function () {
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#collapseButton");
 
+    closeBtn.addEventListener("click", function () {
+      if (window.innerWidth >= 992) {
+        sidebar.classList.toggle("collapse");
+        menuBtnChange();
+      }
+    });
 
-
-    function handleScreenSizeChange() {
-        if (window.innerWidth >= 1300) {
-            functionForLargeScreen();
-            console.log("this only visible in larger screee");
+    function menuBtnChange() {
+      if (window.innerWidth >= 992) {
+        if (sidebar.classList.contains("collapse")) {
+          document.body.style.paddingLeft = "100px";
         } else {
-            functionForSmallScreen();
+          document.body.style.paddingLeft = "260px";
         }
+      }
+    }
+  };
+
+  const gridContainer = document.querySelector(".video-grid");
+  const changeColumnsButton = document.getElementById("collapseButton");
+
+  changeColumnsButton.addEventListener("click", function () {
+    if (window.innerWidth >= 992) {
+      gridContainer.style.gridTemplateColumns = "1fr 1fr 1fr 1fr";
+    }
+  });
+}
+function functionForSmallScreen() {
+
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#collapseButton");
+    
+    // Apply the collapse class on devices with width 786px or larger
+    if (window.innerWidth >= 786) {
+        sidebar.classList.add("collapse");
     }
     
-    function functionForLargeScreen() {
-          window.onload = function(){
+    // Toggle the collapse class when the collapse button is clicked
+    closeBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("collapse");
+    });
        
-   
-   
-     const sidebar = document.querySelector(".sidebar");
-     const closeBtn = document.querySelector("#collapseButton");
-   
-     closeBtn.addEventListener("click",function(){
-         sidebar.classList.toggle("collapse")
-         menuBtnChange()
-     })
-   
-     function menuBtnChange(){
-       if(sidebar.classList.contains("collapse")){
-           document.body.style.paddingLeft="100px";
-       }else{
-           document.body.style.paddingLeft="260px";
-       }
-   }
-   
-   }
-       
-   const gridContainer = document.getElementById('gridContainer');
-   const changeColumnsButton = document.getElementById('collapseButton');
-   let columns = 3; // Initial number of columns
-   
-   changeColumnsButton.addEventListener('click', function() {
-       columns = columns === 3 ? 4 : 3; // Toggle between 3 and 4 columns
-       gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-   });
-   
-       }
+     
 
-       // Initial call to handle the screen size on page load
+}
+// Initial call to handle the screen size on page load
 handleScreenSizeChange();
 
 // Listen for window resize events and update functions accordingly
-window.addEventListener('resize', handleScreenSizeChange);
+window.addEventListener("resize", handleScreenSizeChange);
 
 //collapse action for smalle devices
+const firstGroup = document.querySelector(".firstGroup");
+const secondGroup = document.querySelector(".secondGroup");
 function show() {
-    document.getElementById("mySidebar").style.width = "100%";
-    document.getElementById("content").style.marginLeft = "250px";
-    document.getElementById("close-btn").style.visibility="visible";
- }
- function hide() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("content").style.marginLeft = "0";
-    document.getElementById("close-btn").style.visibility="hidden";
- }
+  document.getElementById("header-deafult").style.display = "none";
+  document.getElementById("mySidebar").style.display = "block";
+  window.alert("main menu is going to hide");
+}
+function hide() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("header-deafult").style.display = "grid";
+
+  window.alert("main menu is going to show");
+}
+
+//add previous and next on button list
+
+const labels = document.querySelector(".labels");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+
+let scrollAmount = 0;
+const scrollStep = 200; // Adjust this value based on your design
+
+prevButton.addEventListener("click", function () {
+  scrollAmount -= scrollStep;
+  labels.scrollLeft = scrollAmount;
+});
+
+nextButton.addEventListener("click", function () {
+  scrollAmount += scrollStep;
+  labels.scrollLeft = scrollAmount;
+});
